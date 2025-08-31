8e4834782b416aa0a9a009d84449d378d31fdf1c
neelpala28 <neelpala28@gmail.com>
2025-08-31 12:53:39 +0530

    set ssl cert to true

--- app.py
+++ app.py
@@ -28,6 +28,12 @@ if not database_uri:
 
 app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
+    "connect_args": {
+        "ssl": True  # simple True works with Aiven
+    }
+}
+
 
 
 
